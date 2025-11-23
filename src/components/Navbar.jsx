5a60e2e8334a382f0ed5222d7b300ded8e3c7e8c
Kartikshetty9403 <141901369+Kartikshetty9403@users.noreply.github.com>
2025-11-23 14:53:40 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
       <div className="container flex items-center justify-between">
         <a className="text-xl font-bold text-primary flex items-center" href="#hero">
           <span className="relative z-10">
-            <span className="text-glow text-foreground">Kartik's</span> Portfolio
+            <span className="text-glow text-foreground">Kartik</span> Shetty
           </span>
         </a>
 
